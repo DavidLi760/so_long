0c3cc7f6481f2bcd211549f0d6679c83894fed39
David Li <davli@bess-f1r5s3.clusters.42paris.fr>
2024-06-20 21:36:16 +0200

    Done

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: davli <davli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 16:08:40 by davli             #+#    #+#             */
-/*   Updated: 2024/06/20 19:52:59 by davli            ###   ########.fr       */
+/*   Updated: 2024/06/20 21:35:08 by davli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include "mlx.h"
 
 int	close_win(t_vars *vars)
 {
@@ -19,25 +20,39 @@ int	close_win(t_vars *vars)
 	return (0);
 }
 
-int	esc_window(int keycode, t_vars *vars)
+int	key_press(int keycode, t_vars *vars)
 {
 	if (keycode == 65307)
 		close_win(vars);
-	return (0);
-}
-
-int	key_press(int keycode, t_vars *vars)
-{
-	(void)vars;
-	if (keycode == 119)
-		printf("Touch pressed :%d, w", keycode);
+	else if (keycode >= 0 && keycode < 256)
+	{
+		vars->key_state[keycode] = 1;
+	}
 	return (0);
 }
 
 int	key_release(int keycode, t_vars *vars)
 {
-	(void)keycode;
-	(void)vars;
+	if (keycode >= 0 && keycode < 256)
+		vars->key_state[keycode] = 0;
+	return (0);
+}
+
+int	update(t_vars *vars)
+{
+	vars->update_counter++;
+	if (vars->update_counter >= 100)
+	{
+		if (vars->key_state[119])
+			printf("W, ");
+		if (vars->key_state[115])
+			printf("S, ");
+		if (vars->key_state[97])
+			printf("A, ");
+		if (vars->key_state[100])
+			printf("D, ");
+	vars->update_counter = 0;
+	}
 	return (0);
 }
 
@@ -67,6 +82,7 @@ int	mouse_move(int x, int y, t_vars *vars)
 int	main(void)
 {
 	t_vars	vars;
+	int	i;
 
 	vars.mouse_pressed = 0;
 	vars.mlx = mlx_init();
@@ -78,6 +94,10 @@ int	main(void)
 		free(vars.mlx);
 		return (1);
 	}
+	i = 0;
+	vars.update_counter = 0;
+	while (i < 256)
+		vars.key_state[i++] = 0;
 	vars.img = mlx_xpm_file_to_image(vars.mlx, "block.xpm", &vars.img_width, &vars.img_height);
 	if (!vars.img)
 		exit(1);
@@ -86,9 +106,9 @@ int	main(void)
 	mlx_hook(vars.win, 17, 0, close_win, &vars);
 	mlx_hook(vars.win, 2, 1L << 0, key_press, &vars);
 	mlx_hook(vars.win, 3, 1L << 1, key_release, &vars);
+	mlx_loop_hook(vars.mlx, update, &vars);
 	mlx_hook(vars.win, 4, 1L << 2, mouse_press, &vars);
 	mlx_hook(vars.win, 5, 1L << 3, mouse_release, &vars);
 	mlx_hook(vars.win, 6, 1L << 6, mouse_move, &vars);
-	mlx_hook(vars.win, 2, 1L << 0, esc_window, &vars);
 	mlx_loop(vars.mlx);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: davli <davli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 16:09:06 by davli             #+#    #+#             */
-/*   Updated: 2024/06/20 19:52:45 by davli            ###   ########.fr       */
+/*   Updated: 2024/06/20 21:32:29 by davli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@
 # include "unistd.h"
 # include "stddef.h"
 
+# define w 119
+# define s 115
+# define a 97
+# define d 100
+
 typedef struct s_vars
 {
 	void	*mlx;
@@ -27,6 +32,10 @@ typedef struct s_vars
 	void	*img;
 	int	img_width;
 	int	img_height;
+	int	pos_x;
+	int	pos_y;
+	int	key_state[256];
+	int	update_counter;
 	int	mouse_pressed;
 
 }	t_vars;
